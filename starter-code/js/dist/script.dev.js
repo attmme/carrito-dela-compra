f2c31cfa07ebc5e229dd44074dc109e08c11833d
Oscar <ogomezsts51l@gmail.com>
2020-10-23 10:53:49 +0200

    Modificadas las funciones de JS

---
+++ starter-code/js/dist/script.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var deleteItem = function deleteItem(e) {};
+
+var getPriceByProduct = function getPriceByProduct(itemNode) {};
+
+var updatePriceByProduct = function updatePriceByProduct(productPrice, index) {};
+
+var getTotalPrice = function getTotalPrice() {};
+
+var createQuantityInput = function createQuantityInput() {};
+
+var createDeleteButton = function createDeleteButton() {};
+
+var createQuantityNode = function createQuantityNode() {};
+
+var createItemNode = function createItemNode(dataType, itemData) {};
+
+var createNewItemRow = function createNewItemRow(itemName, itemUnitPrice) {};
+
+var createNewItem = function createNewItem() {};
+
+addEventListener = ('load', function () {});
--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,3 +1,107 @@
+/* CREAR PRODUCTO -> Ver que hacer con el fichero json que ya hay productos
+
+Función encargada de crear cada uno de los productos, se recibe por parámetros el Nombre, Precio y cantidad en Stock.
+Cuando se ejecute, tiene que hacer un push a un un array de objetos y guardarlo en el local storage en el siguiente formato:
+
+producto = [{
+    id: `${idProducto}`,
+    nombre: `${nombreProducto}`,
+    cantidad: `${cantidadProducto}`,
+    precio: `${precioProducto}`,
+}];
+
+Después ejecuta la función de visualizar producto para actualizar.
+*/
+const createProduct = (n, p, s) => {
+
+}
+
+
+/* BORRAR PRODUCTO -> Ver que hacer con el fichero json que ya hay productos
+
+Función encargada de borrar los productos indicados, se recibe por parametro la id del producto
+y se borra del local storage, después ejecuta la función de visualizar producto para actualizar.
+*/
+const deleteProduct = (id) => {
+
+}
+
+
+/* VISUALIZAR PRODUCTO
+
+Función encargada de visualizar un producto (vista del vendedor)
+Esta función solo se ejecutará después de cada modificación de producto (añadir o borrar producto de la "base de datos").
+*/
+const showProduct = () => {
+
+}
+
+
+/* AÑADIR PRODUCTO AL CARRITO
+
+        Por definir
+
+*/
+const addProduct = () => {
+
+}
+
+
+/* BORRAR PRODUCTO DEL CARRITO
+
+            Por definir
+
+*/
+const removeProduct = () => {
+
+}
+
+
+/* VISUALIZAR CARRITO
+
+        Por definir
+
+*/
+const showCart = () => {
+
+}
+
+
+/* CALCULAR PRECIO TOTAL CARRITO
+
+        Por definir
+
+*/
+const calcTotalPrice = () => {
+
+}
+
+/* COMPROBACIÓN LIMITE DE STOCK
+
+        Por definir
+
+*/
+const checkStock = () => {
+
+}
+
+
+/* REALIZAR LA COMPRA
+
+        Por definir
+
+*/
+const completePurchase = () => {
+
+}
+
+
+
+
+
+/*
+Funciones ya hechas, mirar si nos sirve alguna
+
 const deleteItem = (e) =>{
 
 }
@@ -40,3 +144,4 @@ const createNewItem = () =>{
 
 addEventListener = ('load',  ()=>{
 });
+ */
