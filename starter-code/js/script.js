bc8bb6d6eb29ed882034be6c57ca8858002c0b2e
Jordi <jordi.enriquez@student.bit.es>
2020-10-23 13:07:02 +0200

    get json data

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,3 +1,23 @@
+const myRequest = new Request('../data/products.json');
+
+
+var g_dataObjects ;
+window.onload = function () {
+
+        loadData();
+        
+}
+
+function loadData() {
+        fetch(myRequest)
+        .then(response => response.json())
+        .then(l_data => {
+                console.log(l_data)
+                g_dataObjects = l_data;
+        });
+}
+
+
 /* CREAR PRODUCTO -> Ver que hacer con el fichero json que ya hay productos
 
 Función encargada de crear cada uno de los productos, se recibe por parámetros el Nombre, Precio y cantidad en Stock.
---
+++ starter-code/js/vendedor.js
@@ -0,0 +1,18 @@
+const myRequest = new Request('../data/products.json');
+
+
+var g_dataObjects ;
+window.onload = function () {
+
+        loadData();
+        
+}
+
+function loadData() {
+        fetch(myRequest)
+        .then(response => response.json())
+        .then(l_data => {
+                console.log(l_data)
+                g_dataObjects = l_data;
+        });
+}
